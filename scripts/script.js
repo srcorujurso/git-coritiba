ad873d2f4d73b88284e36dfb2226fb133699bfd6
tilapiaboss <thiaguinhoreinhardt@hotmail.com>
2025-01-04 22:04:06 -0300

    alteração no arquivo script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,10 @@ var txt1 = "What a very";
 var txt2 = "nice day";
 console.log(txt1 + " " + txt2)
 
+<<<<<<< HEAD
 console.log("EU ODEIO O VASCO")
 wefiwieufiwef
+=======
+
+console.log("eu odeio uva passa")
+>>>>>>> dfbd4c5 (alteração no arquivo script.js)
