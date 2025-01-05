8e4d066e316e8235f904a32d35a96780871b1eee
srcorujurso <leandro.canestraro@outlook.com.br>
2025-01-04 22:27:53 -0300

    resolving conflicts

--- scripts/script.js
+++ scripts/script.js
@@ -2,15 +2,9 @@ var txt1 = "What a very";
 var txt2 = "nice day";
 console.log(txt1 + " " + txt2)
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 console.log("EU ODEIO O VASCO")
 wefiwieufiwef
-=======
 
 console.log("eu odeio uva passa")
->>>>>>> dfbd4c5 (alteração no arquivo script.js)
-=======
 
 console.log("eu odeio uva passa")
->>>>>>> dfbd4c500a5e2d26334db5558bcc82cc8f76f077
